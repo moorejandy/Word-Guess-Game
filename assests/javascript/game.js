0069cbb39551a1cc6a38d0902f091ad24da0da09
Andy Moore <moorejandy@gmail.com>
2019-10-26 00:03:14 -0400

    pushing updates to js file, now code selects random word from array, displays _'s equal to length of words, and determines user keystrokes

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,21 +1,50 @@
-var nameOne = ["g", "a", "n", "d", "h", "i"];
 
-var wins = 0;
-var losses = 0;
+    var celebNames = ["Gandhi", "MLKJr", "Rumi", "Obama"];  //the array of celeb names to guess
+    var wins = 0;                                            // var to store # of wins   
+    var losses = 0;                                          //var to store losses   
 
-var celNames = ["GANDHI", "MLK JR", "OBAMA", "AQUINAS"];
-var guessesRemain = 12;
-var guessedLetters = [""]
+    var guessesRemain = 12;                                  // keeps track of attempts remaining
+    var guessedLetters = [];                                  //stores the letters guessed 
+    var guessWord = [];                                     //builds the word that is being guessed
 
-var directionsText = document.getElementById("directions-text");
-var wordText = document.getElementById("word-text");
-var letterGuess = document.getElementById("letter-guess");
-var guessRemain = document.getElementById("guess-remain");
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
+    var directionsText = document.getElementById("directions-text");    //link to directions paragraph
+    var currentWord = document.getElementById("current-word");                     //link to the current word selected at random
+    var letterGuess = document.getElementById("letter-guess");       //place to disply letters guessed
+    var guessRemain = document.getElementById("guess-remain");
+    var winsText = document.getElementById("wins-text");
+    var lossesText = document.getElementById("losses-text");
+    var spaces;
 
-document.onkeyup = function (event) {
-    var userGuess = event.key;
+    // Selects word randomly from names array
+    var wordIndex = celebNames[Math.floor(Math.random() * celebNames.length)];
+    console.log("current word " + wordIndex);
 
-    letterGuess.textContent = "You chose: " + userGuess;
-};
+    //Display _ equal to number of characters in chose word
+
+        var currentWord = wordIndex;
+        var spaces = [];
+
+        function createUnderScore() {
+            for (var i = 0; i < currentWord.length; i++){
+                spaces.push("_");
+            }
+            return spaces;
+        }
+        
+        console.log(createUnderScore());
+
+//Determine Which key was pressed by player
+
+        document.addEventListener("keypress", (event) => {
+            var keyCode = event.keyCode;
+            var keyWord = String.fromCharCode(keyCode);
+            if(currentWord.indexOf(keyWord) > -1) {
+                console.log(true);
+                console.log(keyWord);
+            }
+        });
+        
+        for (var i = 0; i < celebNames.length; i++){
+       
+    }
+ 
